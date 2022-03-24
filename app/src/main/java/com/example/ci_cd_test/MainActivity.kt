316da286ea00b77a32207751876c8f616d029b16
Ahmed.hamdy <ahmed.saadeldin@aboughalymotors.com>
2022-03-24 16:10:56 +0200

    Fixing directory name

--- app/src/main/java/com/example/ci_cd_test/MainActivity.kt
+++ app/src/main/java/com/example/ci_cd_test/MainActivity.kt
@@ -7,6 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
